02261b392ac2ddb4c2466d8336ecbc6a40c1bb31
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-16 23:03:15 +0330

    LeftSideNav separation of concern implemented

---
+++ app/ui/dashboard/MiniDrawer/LeftSideNav.tsx
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MuiDrawer from '@mui/material/Drawer';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import { drawerWidth } from '.';
+import DrawerHeader from './common/DrawerHeader';
+import HomeIcon from '@mui/icons-material/Home';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import Groups2Icon from '@mui/icons-material/Groups2';
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
+
+const sideBarTopIcons = [
+  { text: 'Home', Icon: HomeIcon },
+  { text: 'Invoices', Icon: FileCopyIcon },
+  { text: 'Customers', Icon: Groups2Icon },
+];
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const LeftSideNav = ({ isOpen, onClose }: Props) => {
+  const theme = useTheme();
+
+  return (
+    <Drawer variant="permanent" open={isOpen}>
+      <DrawerHeader>
+        <IconButton onClick={() => onClose()}>
+          {theme.direction === 'rtl' ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {sideBarTopIcons.map(({ text, Icon }) => (
+          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: isOpen ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: isOpen ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {<Icon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: isOpen ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      {/* <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {index % 2 === 0 ? <HomeIcon /> : <FileCopyIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List> */}
+    </Drawer>
+  );
+};
+
+export { LeftSideNav };
