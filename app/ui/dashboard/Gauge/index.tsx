e246819f50d8f7e144969d00bfa8a7e1e533d410
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-18 14:32:19 +0330

    some useful links for showing charts added

---
+++ app/ui/dashboard/Gauge/index.tsx
@@ -0,0 +1,14 @@
+// https://echarts.apache.org/examples/en/editor.html?c=custom-gauge
+// https://echarts.apache.org/examples/en/editor.html?c=pie-alignTo&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=mix-line-bar&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=gauge-grade&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=gauge-temperature&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=dataset-link&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=pie-nest
+// https://echarts.apache.org/examples/en/editor.html?c=pie-roseType-simple&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=bar-race-country
+// https://echarts.apache.org/examples/en/editor.html?c=multiple-y-axis&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=calendar-charts&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=pie-parliament-transition&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=bar-polar-stack&lang=ts
+// https://echarts.apache.org/examples/en/editor.html?c=bar-rich-text&lang=ts
