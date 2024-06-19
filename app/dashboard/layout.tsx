2acbabb3d9b15d8dea3020819de521029c6bff18
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-19 16:11:22 +0330

    dashboard layout div > replace > MUI box

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,25 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+// import SideNav from '@/app/ui/dashboard/sidenav';
 import MiniDrawerClient from '../ui/dashboard/MiniDrawer/MiniDrawerClient';
+import { Box } from '@mui/material';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    // <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <Box
+      sx={
+        {
+          // display: 'flex',
+          // height: '100vh',
+          // flexDirection: { xs: 'column', md: 'row', overflow: { md: 'hidden' } },
+        }
+      }
+    >
       <MiniDrawerClient>{children}</MiniDrawerClient>
-      {/* <div className="w-full flex-none md:w-64">
-        <SideNav />
+    </Box>
+/*  <div className="w-full flex-none md:w-64">
+          <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div> */}
-    </div>
+    <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div> */
+    // </div>
   );
 }
