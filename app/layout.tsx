e3095b96292e73cf807834458861535108923401
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-20 00:55:09 +0330

    baseLight theme added to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import "@/app/ui/global.css"
-import { inter } from "./ui/fonts";
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { baseLightTheme } from '@/utils/theme/DefaultColors';
 
 export default function RootLayout({
   children,
@@ -8,7 +11,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider theme={baseLightTheme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
